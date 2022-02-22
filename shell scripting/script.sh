59a87d2b98bbadca5012f4c2d6f24d089f7ab28d
VallepuSaiMounika <saimounikavallepu24@gmail.com>
2022-02-22 15:30:08 +0530

    lines added more v1

--- shell scripting/script.sh
+++ shell scripting/script.sh
@@ -3,7 +3,7 @@
 echo -e " HEY PILLAGADA,\n YENDHIROY PILLAGADA"
 
 Name=$gorjella
-echo Name=&Name
-Date=date +%F
-echo $Date
+echo Name=$Name
+Date=$(date +%F)
+echo -e "$Date,\t\tThis is your day"
 
